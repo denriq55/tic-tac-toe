fb733e7564df5026e861f414887a59f69778a35f
denriq55 <142200008+denriq55@users.noreply.github.com>
2024-12-17 07:47:03 -0800

    add dialog to get players' names, add start game button

--- script.js
+++ script.js
@@ -16,9 +16,26 @@ const game = {
         [6, 4, 2]
         ],
     
-  
+    startGame() {
+        const startDialog = document.querySelector("#start")
+        const startButton = document.querySelector("#start-button");
 
-    getPlayers(playerOneName, playerTwoName) {
+        startDialog.showModal();
+
+        startButton.addEventListener("click", () => {
+        const playerOneName = document.querySelector("#p1name").value
+        const playerTwoName = document.querySelector("#p2name").value
+        
+        this.getPlayers( playerOneName, playerTwoName );
+        this.createGameBoard();
+
+        startDialog.close()
+        });
+
+    },
+    
+
+    getPlayers(playerOneName, playerTwoName ) {
          this.players = [
             {
                 name: playerOneName,
@@ -32,11 +49,12 @@ const game = {
         
         this.activePlayer = this.players[0]
         
-        },
+    },
         
         
 
     createGameBoard() {
+        
         const board = document.querySelector(".board");
         for (let i = 0; i < 9; i++) {
             
@@ -109,100 +127,4 @@ const game = {
 }
 
 
-
-
-game.getPlayers("Iris", "Dyan")
-game.createGameBoard()
-
-
-
-/*
-
-function getPlayers() {
-    return players = [
-        {
-            name: "Dyan",
-            marker: "X"
-        },
-
-        {
-            name: "Iris",
-            marker: "O"
-        }
-]
-
-}
-
-getPlayers();
-
-
-
-const board = document.querySelector(".board")
-const arrayCells = [];
-const winningCombination = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    
-
-    [0, 4, 8],
-    [6, 4, 2]
-]
-
-function createGameboard()  {
-        
-        for (let i = 0; i < 9; i++) {
-            
-            const cell = document.createElement("button");
-            cell.classList.add("cells");
-            board.appendChild(cell);
-            
-            arrayCells.push(cell);
-
-            cell.addEventListener("click", (e) => {
-                e.target.textContent = activePlayer.marker
-                switchPlayer();
-                checkWinner();
-                })
-
-            const checkWinner = () => {
-                for (let i = 0; i < winningCombination.length; i++) {
-                    const [a, b, c] = winningCombination[i];
-
-                    if (arrayCells[a].textContent !== '' && 
-                        arrayCells[a].textContent === arrayCells[b].textContent &&
-                        arrayCells[a].textContent === arrayCells[c].textContent
-                      ) {
-                        console.log(` is the winner!`) 
-                      }
-                }
-            }
-
-                
-            }
-
-        let activePlayer = players[0];
-
-        const switchPlayer = () => {
-          if (activePlayer === players[0]) {
-                activePlayer = players[1]
-            } else {
-                    activePlayer = players[0]
-                    }
-                    console.log(activePlayer); 
-            }
-
-        
-        
-}
-
-        
-        
-     
-
-    createGameboard();
-*/
+game.startGame()
